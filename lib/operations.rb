357ff754fbd7d77053dcd63a2cbb941bba660622
Gilbert Torchon <gilbert@noukod.com>
2018-09-17 16:46:12 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-	speed > 60 && speed < 40 ? false : speed > 60 || speed > 40 : true
+	speed>60 && speed<40 ? false : speed>60 || speed>40:true
 end
 
 
 
 def not_safe?(speed)
-
+	
 end
+	
+
+
